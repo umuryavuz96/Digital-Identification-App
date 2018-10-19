cf0a873d13565b18db8911c4cca3a1493b8a835f
mdogan13 <mdogan13@ku.edu.tr>
2018-10-19 20:03:44 +0300

    IDScanActivity.

---
+++ app/src/main/java/com/example/murat/m_onboarding/IDScanActivity.java
@@ -0,0 +1,13 @@
+package com.example.murat.m_onboarding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IDScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idscan);
+    }
+}
