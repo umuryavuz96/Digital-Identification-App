9792f68fc75c3a7f3a3890bb056a8b22aeb1eb53
Umur Yavuz <umur@binnaz.com>
2018-10-21 17:49:38 +0300

    Classses and XML layouts are created (Not Filled)

---
+++ app/src/main/java/activities/FaceScanActivity.java
@@ -0,0 +1,15 @@
+package activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.murat.m_onboarding.R;
+
+public class FaceScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idscan);
+    }
+}
