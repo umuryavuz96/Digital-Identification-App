dcdb96dea78d3dfd49a9dff42c8998cdd489812f
mdogan13 <mdogan13@ku.edu.tr>
2018-11-22 16:07:34 +0300

    checksum algorithm implemented

---
+++ app/src/main/java/utils/IDChecksum.java
@@ -0,0 +1,32 @@
+package utils;
+
+
+public class IDChecksum {
+
+    public IDChecksum(){
+
+    }
+
+    public boolean validify(String id){
+
+        String[] chars=id.split("");
+
+        boolean isElevenDigits = (chars.length==11);
+        boolean isFirstDigitZero =(chars[0]=="0");
+
+        int x = 7*(Integer.parseInt(chars[0])+ Integer.parseInt(chars[2])+Integer.parseInt(chars[4])+Integer.parseInt(chars[6])+Integer.parseInt(chars[8]));
+        int y = (Integer.parseInt(chars[1])+ Integer.parseInt(chars[3])+Integer.parseInt(chars[5])+Integer.parseInt(chars[7])+Integer.parseInt(chars[9]));
+
+        boolean canGetDigitTen  = (((x-y)%10) == Integer.parseInt(chars[9]));
+        int sum =0;
+        for (int i =0; i<chars.length-1; i++){
+            sum=sum+ Integer.parseInt(chars[i]);
+        }
+        boolean canGetDigitEleven= (sum%10 == Integer.parseInt(chars[10]));
+        boolean isLastDigitEven = (Integer.parseInt(chars[10])%2==0);
+        if(isElevenDigits&&!isFirstDigitZero&&isLastDigitEven&&canGetDigitTen&&canGetDigitEleven){
+            return true;
+        }
+        return false;
+    }
+}
