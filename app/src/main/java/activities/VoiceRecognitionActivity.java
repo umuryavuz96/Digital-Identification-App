5594dfd5a402cd37224db70ff68ff4cb133221df
Sinan <syaman14@ku.edu.tr>
2018-11-22 15:28:29 +0300

    Voice Recognition UPDATE

--- app/src/main/java/activities/VoiceRecognitionActivity.java
+++ app/src/main/java/activities/VoiceRecognitionActivity.java
@@ -1,31 +1,144 @@
 package activities;
 
+import android.Manifest;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import com.example.murat.m_onboarding.R;
 
 import utils.CameraPreview;
 
 public class VoiceRecognitionActivity extends AppCompatActivity {
-    private Button nextButton3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_recognition);
-        nextButton3=findViewById(R.id.nextButton3);
 
-        nextButton3.setOnClickListener(new View.OnClickListener() {
+        checkPermission();
+        final EditText editText = findViewById(R.id.editText);
+        final EditText displayOutput = findViewById(R.id.displayOutput);
+        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                "tr-TR");
+
+
+        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
             @Override
-            public void onClick(View view) {
+            public void onReadyForSpeech(Bundle bundle) {
 
-                Intent goToSign = new Intent(getBaseContext(), SignActivity.class);
-                startActivity(goToSign);
-                finish();
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                //getting all the matches
+                ArrayList<String> matches = bundle
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+
+                String bestFit = matches.get(0);
+
+
+                //displaying the first match
+                if (matches != null)
+                    editText.setText(matches.get(0));
+
+                for(String m : matches){
+                    if(displayOutput.getText().toString().equalsIgnoreCase(editText.getText().toString())){
+                        Toast.makeText(VoiceRecognitionActivity.this,"Right on!",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
             }
         });
+
+        findViewById(R.id.button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizer.stopListening();
+                        editText.setHint("You will see input here");
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        editText.setText("");
+                        editText.setHint("Listening...");
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
+
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
 }
